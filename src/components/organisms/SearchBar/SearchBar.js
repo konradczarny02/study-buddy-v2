67396e90dfd3866679bd56f9583ccae8f29073f0
Konrad Czarny <konradcz@MacBook-Air-Konrad.local>
2021-12-11 21:35:19 +0100

    created news section and connected with datocms via axios

---
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SearchBarWrapper, StatusInfo } from './SearchBar.styles';
+import { Input } from 'components/atoms/Input/Input';
+
+const SearchBar = () => {
+  return (
+    <SearchBarWrapper>
+      <StatusInfo>
+        <p>Logged as:</p>
+        <p>
+          <strong>Teacher</strong>
+        </p>
+      </StatusInfo>
+      <Input />
+    </SearchBarWrapper>
+  );
+};
+
+export default SearchBar;
